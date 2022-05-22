538a7c6dd0d22b9969ec63e7f752b6c5e09e4106
karan <karan.mishra_cs19@gla.ac.in>
2022-05-22 21:48:46 +0530

    initial commit

--- react-gym-website/src/App.js
+++ react-gym-website/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import JoinUs from "./components/JoinUs/JoinUs";
+import Features from "./components/Features/Features";
+import Sale from "./components/Sale/Sale";
+import Pricing from "./components/Pricing/Pricing";
+import Gallery from "./components/Gallery/Gallery";
+import Trainers from "./components/Trainers/Trainers";
+import Summer from "./components/Summer/Summer";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar />
+      <Hero />
+      <JoinUs />
+      <Features />
+      <Sale />
+      <Pricing />
+      <Gallery />
+      <Trainers />
+      <Summer />
+      <Footer />
+    </div >
   );
 }
 
