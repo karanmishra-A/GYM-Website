16f923480674b2daea6f2eee812e18d39e080c36
karan <karan.mishra_cs19@gla.ac.in>
2022-05-22 21:55:20 +0530

    adding pricing section

---
+++ react-gym-website/src/components/JoinUs/JoinUs.js
@@ -0,0 +1,71 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import JoinUs1 from "../../images/JoinUs1.png";
+import JoinUs2 from "../../images/JoinUs2.png";
+import JoinUsInfo from "./JoinUsInfo";
+
+const JoinUs = () => {
+    return (
+        <section className="joinUs" css={styles}>
+            <div className="wrap">
+                <div className="img1">
+                    <JoinUsInfo title="FOR WOMEN"/>
+                </div>
+            </div>
+            <div className="wrap">
+                <div className="img2">
+                    <JoinUsInfo title="FOR MEN"/>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+const styles = css`
+    width: 100%;
+    display: flex;
+    .wrap {
+        overflow: hidden;
+        width: 50%;
+        .img1 {
+            min-height: 50vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: all .8s;
+            background: url('${JoinUs1}') no-repeat center/cover;
+            &:hover {
+                transform: scale(1.2);
+            }
+        }
+    }
+    .wrap {
+        overflow: hidden;
+        width: 50%;
+        margin-top:30px;
+        .img1{
+            min-height:70vh;
+        }
+        .img2 {
+            min-height: 70vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: all .8s;
+            background: url('${JoinUs2}') no-repeat center/cover;
+            &:hover {
+                transform: scale(1.2);
+            }
+        }
+    }
+
+    @media(max-width: 990px) {
+        flex-direction: column;
+        .wrap {
+            width: 100%;
+        }
+    }
+`
+
+export default JoinUs;
---
+++ react-gym-website/src/components/Pricing/Pricing.js
@@ -0,0 +1,59 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import Container from "../Global/Container";
+import Heading from "../Global/Heading/Heading";
+import PricingCard from "./PricingCard";
+
+const Pricing = () => {
+    return (
+        <section className="pricing" css={styles}>
+            <Container>
+                <Heading title="OUR PRICING"/>
+                <div className="wrapper">
+                    <PricingCard cardTitle="BEGINNER" cardPricing="Rs 500/month" />
+                    <PricingCard cardTitle="EXPERT" cardPricing="Rs 1000/month" />
+                    <PricingCard cardTitle="PRO" cardPricing="Rs 1500/month" />
+                </div>
+            </Container>
+        </section>
+    )
+}
+
+const styles = css`
+    width: 100%;
+    background: #000;
+    .container {
+        padding: 150px 0;
+        max-width: 1200px;
+        .wrapper {
+            padding: 80px 0 0 0;
+            display: flex;
+            justify-content: space-between;
+        }
+    }
+    @media (max-width: 730px) {
+        .container {
+            .wrapper {
+                flex-direction: column;
+                flex-wrap: wrap;
+                .pricingCard {
+                    margin: 10px auto;
+                }
+            }
+        }
+    }
+
+    @media (min-width: 731px) and (max-width: 1320px) {
+        .container {
+            .wrapper {
+                flex-wrap: wrap;
+                .pricingCard {
+                    max-width: 33%;
+                    margin: 20px 0 0 0;
+                }
+            }
+        }
+    }
+`
+
+export default Pricing;
